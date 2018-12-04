52f17ea289478376317d014d7a4e77529f92e40c
cusin <37367299+imsuchan@users.noreply.github.com>
2018-12-04 21:53:04 +0900

    Create cat.js

---
+++ cat.js
@@ -0,0 +1,51 @@
+var data = [
+    {
+        "id": "1", "name": "놀숲","photo": "https://post-phinf.pstatic.net/MjAxODAxMTVfOCAg/MDAxNTE2MDIwNDQzMTc4.AgYk-HV_ESKLntzSOMK0Qb7r1hw8qZO6Bwk_elwS74Yg.DgWdOlpOVp0Q3jo2WsBV9K1UeCFeYtEos77zisRcJd4g.JPEG/1.jpg?type=w1200"
+    }
+    ,
+    {
+        "id": "2", "name": "발리네즈","photo": "http://mblogthumb1.phinf.naver.net/20160807_280/aplusah7582_1470537738026Hr5xy_JPEG/image_9548945511470536715966.jpg?type=w800"
+    },
+    {
+        "id": "3", "name": "라팜","photo": "https://mblogthumb-phinf.pstatic.net/20141003_239/botos__1412318968083KYBiP_JPEG/019.jpg?type=w2"
+    },
+    {
+        "id": "4", "name": "렉돌","photo": "https://mblogthumb-phinf.pstatic.net/20141003_118/botos__1412320092275Vq0Wx_JPEG/021.jpg?type=w2"
+    },
+    {
+        "id": "5", "name": "러시안블루","photo": "https://mblogthumb-phinf.pstatic.net/20141003_83/botos__1412320999489qhnjr_JPEG/028.jpg?type=w2"
+    },
+    {
+        "id": "6", "name": "맹크스","photo": "https://mblogthumb-phinf.pstatic.net/20141003_293/botos__1412322452893x6WdA_JPEG/036.jpg?type=w2"
+    },
+    {
+        "id": "7", "name": "메인쿤","photo": "https://mblogthumb-phinf.pstatic.net/20141003_71/botos__1412325310932mawDP_JPEG/037.jpg?type=w2"
+    },
+    {
+        "id": "8", "name": "발리네즈","photo": "https://mblogthumb-phinf.pstatic.net/20141003_64/botos__1412327341823fhto5_JPEG/045.jpg?type=w2"
+    },
+    {
+        "id": "9", "name": "버만","photo": "https://mblogthumb-phinf.pstatic.net/20141003_10/botos__1412327977735Ij1qX_JPEG/050.jpg?type=w2"
+    },
+    {
+        "id": "10", "name": "버미즈","photo": "https://postfiles.pstatic.net/20141020_93/botos__14137846751148XWQT_JPEG/002.jpg?type=w1"
+    },
+    {
+        "id": "11", "name": "스코티시폴드","photo": "http://www.segye.com/content/image/2017/05/11/20170511001324_0.jpg"
+    },
+    {
+        "id": "12", "name": "페르시안","photo": "https://post-phinf.pstatic.net/MjAxNzA1MjlfMTMw/MDAxNDk2MDQyODMxNzEy.wlf65amnwCP9uTvXiborN2HgAFxRdWZdUdadnJxgvPAg.zusc_v1sphxTZddKf09c1hmgRgFtTVylBrd2YwNipfkg.PNG/20170529_162613.png?type=w1200"
+    },
+    {
+        "id": "13", "name": "먼치킨","photo": "https://post-phinf.pstatic.net/MjAxODAyMDVfMjYy/MDAxNTE3ODMxNzA4OTYy.At0bjIVgpZhqo8kK_gbJ0ynCJCMYGcyp5nmi0bMdXFAg._qOqOGjNa23z4UIGtDe96f-ZDyElAXkkz_DzRsJDWGkg.JPEG/1.jpg?type=w1200"
+    },
+    {
+        "id": "14", "name": "시베리아숲","photo": "https://post-phinf.pstatic.net/MjAxNzA2MTJfMTgy/MDAxNDk3MjU2MTkyMDAx.6ntzCuGmxqn-HHSFphW0YtFF-0i1J5VpYxDl-yIfHoog.UuvUURroK5G-AY84EC-WQjWY7F7iVAM83x_3I7hlbKUg.JPEG/image_1179953391497245373394.jpg?type=w1200"
+    },
+    {
+        "id": "15", "name": "샤르트뢰","photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYIbch6JTRpWs9Zc_WmuwgIPsxFgFWfg7Vl7QhxtX7xSVJZIKN"
+    },
+    {
+        "id": "16", "name": "타이캣","photo": "https://www.worldlifeexpectancy.com/images/a/d/c/b/thai/thai_1.jpg"
+    }
+]
